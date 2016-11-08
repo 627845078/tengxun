ff031f17fd84dca8d6c7181f76d7e8cd4838c0cf
unknown <>
2016-11-08 16:48:19 +0800

    开始

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name practiceThreeApp
+ * @description
+ * # practiceThreeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('practiceThreeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name practiceThreeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the practiceThreeApp
+ */
+angular.module('practiceThreeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
